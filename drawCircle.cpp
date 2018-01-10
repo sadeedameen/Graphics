8a8e7bf05e82961dfd6c7837ae1d4988d14bb16e
Sadeed Ameen <sadeedameen@gmail.com>
2018-01-10 22:41:57 +0530

    List with structure counting as circle

--- drawCircle.cpp
+++ drawCircle.cpp
@@ -1,88 +1,14 @@
 #include <GL/glut.h>
 #include <iostream>
- 
+#include <list>
 using namespace std;
  
-struct node
-{
-	
-
-}; 
- 
-// This class represents a directed graph using
-// adjacency list representation
 struct node{
-	int x;
-	int y;
-}
-class Graph
-{
-    int V;    // No. of vertices
- 
-    // Pointer to an array containing adjacency
-    // lists
-    list<int> *adj;   
-public:
-    Graph(int V);  // Constructor
- 
-    // function to add an edge to graph
-    void addEdge(int v, int w); 
- 
-    // prints BFS traversal from a given source s
-    void BFS(int s);  
+int data;
+int x;
+int y;
 };
- 
-Graph::Graph(int V)
-{
-    this->V = V;
-    adj = new list<int>[V];
-}
- 
-void Graph::addEdge(int v, int w)
-{
-    adj[v].push_back(w); // Add w to v’s list.
-}
- 
-void Graph::BFS(int s)
-{
-    // Mark all the vertices as not visited
-    bool *visited = new bool[V];
-    for(int i = 0; i < V; i++)
-        visited[i] = false;
- 
-    // Create a queue for BFS
-    list<int> queue;
- 
-    // Mark the current node as visited and enqueue it
-    visited[s] = true;
-    queue.push_back(s);
- 
-    // 'i' will be used to get all adjacent
-    // vertices of a vertex
-    list<int>::iterator i;
- 
-    while(!queue.empty())
-    {
-        // Dequeue a vertex from queue and print it
-        s = queue.front();
-        cout << s << " ";
-        queue.pop_front();
- 
-        // Get all adjacent vertices of the dequeued
-        // vertex s. If a adjacent has not been visited, 
-        // then mark it visited and enqueue it
-        for (i = adj[s].begin(); i != adj[s].end(); ++i)
-        {
-            if (!visited[*i])
-            {
-                visited[*i] = true;
-                queue.push_back(*i);
-            }
-        }
-    }
-}
-
- Graph g(7);
+list<node> nodeList;
 int count=0;
 void circlePlotPoints(int xCenter, int yCenter, int x, int y )
 {
@@ -102,6 +28,7 @@ void circlePlotPoints(int xCenter, int yCenter, int x, int y )
 void drawCircle( int xCenter, int yCenter, int radius )
 {
 	int x = 0, y = radius, p = 1 - radius;
+	node xnode;
 	circlePlotPoints( xCenter, yCenter, x, y );
 	while( x < y )
 	{
@@ -117,8 +44,12 @@ void drawCircle( int xCenter, int yCenter, int radius )
 		}
 		circlePlotPoints(xCenter, yCenter, x, y);
 	}
+	xnode.x=xCenter;
+	xnode.y=yCenter;
+	xnode.data=count++;
+	nodeList.push_back(xnode);
 }
-void drawLine( int xStart, int yStart, int xEnd, int yEnd )
+/*void drawLine( int xStart, int yStart, int xEnd, int yEnd )
 {
 	int a,b,c,d;
 	a=xStart;
@@ -133,7 +64,7 @@ void drawLine( int xStart, int yStart, int xEnd, int yEnd )
 		glVertex2i(a++, c++ );
 	}
 	glEnd();
-}
+}*/
 
 
 void display()
@@ -152,19 +83,20 @@ void mouseClick( int button, int state, int x, int y )
 			
 			drawCircle(x, 500-y, 20);
 			glutSwapBuffers();
-			drawLine(x+20,500-y, x+40,y+40);
-			count++;
-			if (count<7)
-			g.addEdge(0, count);
-			
-			if (count>7) g.BFS(0);
-			
+		//	drawLine(x+20,500-y, x+40,y+40);
+		for (list<node>::iterator it=nodeList.begin(); it != nodeList.end(); ++it)
+			{
+				node xnode=*it;
+				cout<<xnode.data;
+			}
+    			//cout << ' ' << *it;
 		}
 	}
 }
 
 int main(int argc, char **argv)
 {
+	
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
 	glutInitWindowPosition(50,50);
@@ -178,7 +110,9 @@ int main(int argc, char **argv)
 	
 	glutDisplayFunc(display);
 	glutMouseFunc(mouseClick);
+	
 	glutMainLoop();
+	
 	return 0;
 }
 
