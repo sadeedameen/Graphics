71f9e5de5a5ebd04fa59a174a4cd89d5e1aed93c
Sadeed Ameen <sadeed@localhost.localdomain>
2018-03-20 08:04:27 +0530

    circle drawing

---
+++ circle.cpp
@@ -0,0 +1,85 @@
+#include <GL/glut.h>
+#include <iostream>
+#include <string.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <wchar.h>
+
+using namespace std;
+void circlePlotPoints(int xCenter, int yCenter, int x, int y )
+{
+	glColor3f(0.0, 0.0, 0.0);
+	glBegin(GL_POINTS);
+		glVertex2i(xCenter + x, yCenter + y );
+		glVertex2i(xCenter - x, yCenter + y );
+		glVertex2i(xCenter + x, yCenter - y );
+		glVertex2i(xCenter - x, yCenter - y );
+		glVertex2i(xCenter + y, yCenter + x );
+		glVertex2i(xCenter - y, yCenter + x );
+		glVertex2i(xCenter + y, yCenter - x );
+		glVertex2i(xCenter - y, yCenter - x );
+	glEnd();
+}
+
+void drawCircle( int xCenter, int yCenter, int radius )
+{
+
+	int x = 0, y = radius, p = 1 - radius;
+	circlePlotPoints( xCenter, yCenter, x, y );
+	while( x < y )
+	{
+		x++;
+		if( p < 0 )
+		{
+			p += (2 * x) + 1;
+		}
+		else
+		{
+			y--;
+			p += ( 2*(x-y) ) +1;
+		}
+		circlePlotPoints(xCenter, yCenter, x, y);
+	}
+	
+}
+
+void mouseClick( int button, int state, int x, int y )
+{
+	if( button == GLUT_LEFT_BUTTON )
+	{
+		if( state == GLUT_DOWN )
+		{
+			
+			drawCircle(x, 500-y, 20);
+			glutSwapBuffers();
+		}
+	}
+}
+
+void display()
+{
+	glClearColor(1.0, 1.0, 1.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glutSwapBuffers();
+}
+int main(int argc, char **argv)
+{
+	
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowPosition(50,50);
+	glutInitWindowSize(800, 500);
+	glutCreateWindow("BFS");
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0, 800, 0, 500);
+	glutDisplayFunc(display);
+	glutMouseFunc(mouseClick);
+	glutMainLoop();
+	
+	return 0;
+}
+
+
+
+
